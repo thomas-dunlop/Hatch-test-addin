b22c1ee107f42f3d33c2a7650fb55369090e57a1
thomas-dunlop <dunlopt12@gmail.com>
2023-01-17 18:27:09 -0500

    first commit

---
+++ node_modules/custom-functions-metadata/README.md
@@ -0,0 +1,19 @@
+# Custom-Functions-Metadata
+
+This package allows metadata for custom functions to be generated automatically from JSDoc tags and the function parameter types.
+
+## Command-Line Interface
+* [generate](#generate)
+
+#
+
+## generate 
+Generates metadata for custom functions from source code. 
+
+Syntax:
+
+`custom-functions-metadata generate <sourceFile> [outputFile]`
+
+`sourceFile`: path to the source file (.ts or .js).
+`outputFile`: If specified, the metadata is written to the file. Otherwise, it is written to the console.
+
---
+++ src/functions/functions.js
@@ -0,0 +1,43 @@
+﻿/**
+ * @customfunction GetStreamValue
+ * @param {string} streamNumber
+ * @param {string} operatingCase
+ * @param {string} propertyType
+ * @param {string[][]} searchRange
+ * @returns {string} of PropertyValue.
+ */
+export function getStreamValue(streamNumber, operatingCase, propertyType, searchRange) {
+  const streamNumberIndex = findHeaderIndex("StreamNumber", searchRange);
+  const operatingCaseIndex = findHeaderIndex("OperatingCase", searchRange);
+  const propertyTypeIndex = findHeaderIndex("PropertyType", searchRange);
+  const propertyValueIndex = findHeaderIndex("PropertyValue", searchRange);
+
+  const matchingRow = searchRange.find(
+    (element) =>
+      element[streamNumberIndex] === streamNumber &&
+      element[operatingCaseIndex] === operatingCase &&
+      element[propertyTypeIndex] === propertyType
+  );
+
+  if (!matchingRow) {
+    let error = new CustomFunctions.Error(
+      CustomFunctions.ErrorCode.notAvailable,
+      `Could not find PropertyValue for StreamNumber: ${streamNumber}, OperatingCase: ${operatingCase}, PropertyType: ${propertyType}`
+    );
+    throw error;
+  }
+
+  return matchingRow[propertyValueIndex];
+}
+
+const findHeaderIndex = (headerName, searchRange) => {
+  const index = searchRange[0].findIndex((element) => element === headerName);
+  if (index === -1) {
+    let error = new CustomFunctions.Error(
+      CustomFunctions.ErrorCode.invalidValue,
+      `${headerName} not found in header row (first row of searchRange)`
+    );
+    throw error;
+  }
+  return index;
+};
